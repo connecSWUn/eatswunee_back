f77fcfdf66c449afd23b95ed6746f6fc9cd47a95
eunhee <dmsgml9903@gmail.com>
2023-05-13 19:15:15 +0900

    [FEAT] 식당 관련 에러코드 작성  : 식당이 존재하지 않으면 RestaurantNotFoundException 발생

--- src/main/java/com/swulab/eatswunee/global/error/ErrorCode.java
+++ src/main/java/com/swulab/eatswunee/global/error/ErrorCode.java
@@ -3,7 +3,10 @@ package com.swulab.eatswunee.global.error;
 
 public enum ErrorCode {
   INVALID_INPUT_VALUE("유효성 검증 실패",400, -400, "올바르지 않은 형식의 값입니다."),
-  INTERNAL_SERVER_ERROR("서버 에러",500, -500, "서버 에러");
+  INTERNAL_SERVER_ERROR("서버 에러",500, -500, "서버 에러"),
+
+  // 식당
+  RESTAURANT_NOT_FOUND("존재하지 않는 식당", 404, -404, "주어진 식당이 존재하지 않습니다.");
 
 //  private final String type;
 //  private final String instance;
