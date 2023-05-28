b073b263d5e959e5db6e339cda1dcddeddb4094e
eunhee <dmsgml9903@gmail.com>
2023-05-28 21:24:30 +0900

    [CHORE] S3 설정 및 url 제공 서비스 구현

---
+++ src/main/java/com/swulab/eatswunee/global/common/application/service/S3service.java
@@ -0,0 +1,22 @@
+package com.swulab.eatswunee.global.common.application.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.swulab.eatswunee.global.common.applicatioin.port.in.GetImageUrlUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class S3service implements GetImageUrlUseCase {
+
+  private final AmazonS3Client amazonS3Client;
+
+  @Value("${cloud.aws.s3.bucket}")
+  private String bucketName;
+
+  public String getImageUrl(String resource) {
+    return amazonS3Client.getUrl(bucketName, resource).toString();
+  }
+
+}
