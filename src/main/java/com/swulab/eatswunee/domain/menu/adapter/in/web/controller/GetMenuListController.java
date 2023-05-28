6c382801ddc67e0bee7ec77f90f3434999cb4cc2
eunhee <dmsgml9903@gmail.com>
2023-05-28 21:25:07 +0900

    [FEAT] 메뉴 이미지 url 불러오기 기능 구현

--- src/main/java/com/swulab/eatswunee/domain/menu/adapter/in/web/controller/GetMenuListController.java
+++ src/main/java/com/swulab/eatswunee/domain/menu/adapter/in/web/controller/GetMenuListController.java
@@ -4,6 +4,7 @@ import com.swulab.eatswunee.domain.menu.adapter.in.web.controller.dto.response.G
 import com.swulab.eatswunee.domain.menu.application.port.in.GetMenuListUseCase;
 import com.swulab.eatswunee.domain.menu.application.port.in.command.GetMenuListCommand;
 import com.swulab.eatswunee.global.common.adapter.web.in.dto.SuccessResponse;
+import com.swulab.eatswunee.global.common.applicatioin.port.in.GetImageUrlUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GetMenuListController {
 
   private final GetMenuListUseCase getMenuListUseCase;
+  private final GetImageUrlUseCase getImageUrlUseCase;
 
 
   @GetMapping("/gusia/{restaurantId}")
@@ -22,10 +24,12 @@ public class GetMenuListController {
 
     GetMenuListCommand command = getMenuListUseCase.getMenuList(restaurantId);
 
+    getImageUrlUseCase.getImageUrl(command.getMenuCommandList().get(0).getMenuImg());
+    command.getMenuCommandList().forEach(menu -> menu.mapNameToUrl(getImageUrlUseCase.getImageUrl("menu_image/" + menu.getMenuImg())));
+
     GetMenuListResponse response = new GetMenuListResponse(command);
 
     return ResponseEntity.ok(SuccessResponse.create200SuccessResponse(response));
-
   }
 
 }
