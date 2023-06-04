ca108c246e9bffdb1dc23d940315b8e6ec1b63b3
eunhee <dmsgml9903@gmail.com>
2023-06-04 19:56:21 +0900

    [CHORE] 주문할 때 인증 과정 추가

--- src/main/java/com/swulab/eatswunee/domain/order/adapter/in/web/controller/AddOrderController.java
+++ src/main/java/com/swulab/eatswunee/domain/order/adapter/in/web/controller/AddOrderController.java
@@ -6,7 +6,8 @@ import com.swulab.eatswunee.domain.order.application.port.in.AddOrderUseCase;
 import com.swulab.eatswunee.global.common.adapter.web.in.dto.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,10 @@ public class AddOrderController {
 
   private final AddOrderUseCase addOrderUseCase;
 
-  @PostMapping("/gusia/order/save/{userId}")
-  public ResponseEntity addOrderController(@PathVariable Long userId, @RequestBody AddOrderRequest request) {
+  @PostMapping("/gusia/order/save")
+  public ResponseEntity addOrderController(@AuthenticationPrincipal UserDetails userDetails, @RequestBody AddOrderRequest request) {
 
-    Long orderId = addOrderUseCase.addOrder(userId, request.toCommands());
+    Long orderId = addOrderUseCase.addOrder(Long.parseLong(userDetails.getUsername()), request.toCommands());
     AddOrderResponse response = new AddOrderResponse(orderId);
 
     return ResponseEntity.ok(SuccessResponse.create201SuccessResponse(response));
