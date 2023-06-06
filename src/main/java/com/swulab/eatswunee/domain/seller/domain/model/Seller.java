84af6a237946032b190d87cc09d1c4d8807e2c44
eunhee <dmsgml9903@gmail.com>
2023-06-06 19:54:08 +0900

    [ADD] 판매자 객체에 변수 추가

--- src/main/java/com/swulab/eatswunee/domain/seller/domain/model/Seller.java
+++ src/main/java/com/swulab/eatswunee/domain/seller/domain/model/Seller.java
@@ -8,11 +8,25 @@ import lombok.Getter;
 public class Seller {
 
   private Long sellerId;
+  private String sellerName;
+  private String sellerProfileUrl;
+  private String sellerFcmToken;
+  private String LoginId;
+  private String password;
+
   private Restaurant restaurant;
 
   @Builder
-  public Seller(Long sellerId, Restaurant restaurant) {
+  public Seller(Long sellerId, String sellerName, String sellerProfileUrl,
+      String sellerFcmToken, String loginId, String password,
+      Restaurant restaurant) {
     this.sellerId = sellerId;
+    this.sellerName = sellerName;
+    this.sellerProfileUrl = sellerProfileUrl;
+    this.sellerFcmToken = sellerFcmToken;
+    LoginId = loginId;
+    this.password = password;
     this.restaurant = restaurant;
   }
 }
+
