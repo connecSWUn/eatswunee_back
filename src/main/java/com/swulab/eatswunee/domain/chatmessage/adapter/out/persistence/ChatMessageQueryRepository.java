f9487aa003a035e94ab5d45809fed531b44d9170
eunhee <dmsgml9903@gmail.com>
2023-06-12 00:58:45 +0900

    [FEAT] 마지막 메시지 조회 기능 구현

---
+++ src/main/java/com/swulab/eatswunee/domain/chatmessage/adapter/out/persistence/ChatMessageQueryRepository.java
@@ -0,0 +1,35 @@
+package com.swulab.eatswunee.domain.chatmessage.adapter.out.persistence;
+
+import static com.swulab.eatswunee.domain.chatmessage.adapter.out.persistence.jpa.model.QChatMessageJpaEntity.chatMessageJpaEntity;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.swulab.eatswunee.domain.chatroom.application.port.out.command.LastChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ChatMessageQueryRepository {
+
+
+  private final JPAQueryFactory jpaQueryFactory;
+
+
+  public LastChatMessage findLastChatMessage(Long chatRoomId) {
+
+    return jpaQueryFactory
+        .select(Projections.constructor(LastChatMessage.class,
+            chatMessageJpaEntity.createdAt.as("lastChatCreatedAt"),
+            chatMessageJpaEntity.message.as("lastChatMessage")
+            ))
+        .from(chatMessageJpaEntity)
+        .where(
+            chatMessageJpaEntity.chatRoomJpaEntity.chatRoomId.eq(chatRoomId)
+        )
+        .orderBy(chatMessageJpaEntity.createdAt.desc())
+        .fetchFirst();
+
+  }
+
+}
---
+++ src/main/java/com/swulab/eatswunee/domain/chatroom/application/port/out/FindLastChatMessagePort.java
@@ -0,0 +1,9 @@
+package com.swulab.eatswunee.domain.chatroom.application.port.out;
+
+import com.swulab.eatswunee.domain.chatroom.application.port.out.command.LastChatMessage;
+
+public interface FindLastChatMessagePort {
+
+  LastChatMessage findLastChatMessage(Long chatRoomId);
+
+}
