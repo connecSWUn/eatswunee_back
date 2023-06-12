8e3ed0c69016c0e3bbf38ebaa86d89a3687a130b
eunhee <dmsgml9903@gmail.com>
2023-06-13 05:01:40 +0900

    [FIX] @PathVariable 추가

--- src/main/java/com/swulab/eatswunee/domain/user/adapter/in/web/controller/CheckNicknameController.java
+++ src/main/java/com/swulab/eatswunee/domain/user/adapter/in/web/controller/CheckNicknameController.java
@@ -1,12 +1,13 @@
 package com.swulab.eatswunee.domain.user.adapter.in.web.controller;
 
-import com.swulab.eatswunee.domain.user.adapter.in.web.dto.response.CheckNicknameResponse;
+import com.swulab.eatswunee.domain.user.adapter.in.web.dto.response.CheckDeuplicatedResponse;
 import com.swulab.eatswunee.domain.user.application.port.in.CheckNicknameUseCase;
-import com.swulab.eatswunee.domain.user.application.port.in.command.CheckNicknameCommand;
+import com.swulab.eatswunee.domain.user.application.port.in.command.CheckDuplicatedCommand;
 import com.swulab.eatswunee.global.common.adapter.web.in.dto.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,10 +17,10 @@ public class CheckNicknameController {
   private final CheckNicknameUseCase checkNicknameUseCase;
 
   @GetMapping("/mypage/duplicated/{nickname}")
-  public ResponseEntity checkNickname(String nickname) {
+  public ResponseEntity checkNickname(@PathVariable String nickname) {
 
-    CheckNicknameCommand command = checkNicknameUseCase.checkNickname(nickname);
-    CheckNicknameResponse response = new CheckNicknameResponse(command);
+    CheckDuplicatedCommand command = checkNicknameUseCase.checkNickname(nickname);
+    CheckDeuplicatedResponse response = new CheckDeuplicatedResponse(command);
 
     return ResponseEntity.ok(SuccessResponse.create200SuccessResponse(response));
   }
