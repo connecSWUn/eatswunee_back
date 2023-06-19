4da22782d695ee283a2a688bf0bf4e093fc3a8df
eunhee <dmsgml9903@gmail.com>
2023-06-19 21:11:07 +0900

    [CHORE] url 추가

--- src/main/java/com/swulab/eatswunee/domain/chatroom/adapter/in/web/controller/GetUserChatRoomListController.java
+++ src/main/java/com/swulab/eatswunee/domain/chatroom/adapter/in/web/controller/GetUserChatRoomListController.java
@@ -4,6 +4,7 @@ import com.swulab.eatswunee.domain.chatroom.adapter.in.dto.response.UserChatRoom
 import com.swulab.eatswunee.domain.chatroom.adapter.in.web.controller.command.UserChatRoomCommand;
 import com.swulab.eatswunee.domain.chatroom.application.port.in.GetUserChatRoomListUseCase;
 import com.swulab.eatswunee.global.common.adapter.web.in.dto.SuccessResponse;
+import com.swulab.eatswunee.global.common.application.port.in.GetImageUrlUseCase;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class GetUserChatRoomListController {
 
   private final GetUserChatRoomListUseCase getUserChatRoomListUseCase;
+  private final GetImageUrlUseCase getImageUrlUseCase;
 
   @GetMapping("/user/chatroom/list")
   public ResponseEntity GetUserChatRoomList(@AuthenticationPrincipal UserDetails userDetails) {
     List<UserChatRoomCommand> commands = getUserChatRoomListUseCase.getUserChatRoomList(Long.parseLong(userDetails.getUsername()));
+    commands.forEach(
+        command -> {
+          command.mapToUrl(getImageUrlUseCase.getImageUrl("user_profile/" + command.getSenderProfileImgUrl()));
+        }
+    );
     UserChatRoomListResponse response = new UserChatRoomListResponse(commands);
     return ResponseEntity.ok(SuccessResponse.create200SuccessResponse(response));
   }
