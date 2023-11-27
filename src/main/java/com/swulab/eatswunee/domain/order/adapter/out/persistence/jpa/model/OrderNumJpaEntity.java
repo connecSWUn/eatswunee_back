31be99cc1a0bfaa125ccfa43277266ce09500891
eunhee <dmsgml9903@gmail.com>
2023-11-27 16:29:51 +0900

    fix : 주문 번호 로직 수정

--- src/main/java/com/swulab/eatswunee/domain/order/adapter/out/persistence/jpa/model/OrderNumJpaEntity.java
+++ src/main/java/com/swulab/eatswunee/domain/order/adapter/out/persistence/jpa/model/OrderNumJpaEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "order_num")
 public class OrderNumJpaEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
